5867d59e0435f324b0dfc4327cc6c825fad9bc81
amaydeveloper <amaydeveloper@MacBook-Pro-tilhrende-amaydeveloper.local>
2022-05-04 12:14:02 +0200

    navbar implemented successfully alhamdulillah

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div>
-      <h1> hello world </h1>
+      <Navbar />
     </div>
   );
 };
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Search } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+
+const Container = styled.div`
+  height: 60px;
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Left = styled.div`
+  widht:flex: 1;
+  display:flex;
+  align-items:center;
+`;
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-itrms: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+const Center = styled.div`
+  widht:flex: 1;
+  text-align:center;
+`;
+const Right = styled.div`
+  widht: 1;
+  display: flex;
+`;
+const Input = styled.input`
+  border: none;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>En</Language>
+          <SearchContainer>
+            <Input />
+            <Search style={{ color: 'gray', fontSize: '16px' }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>E-Commerce</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Sign In</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
