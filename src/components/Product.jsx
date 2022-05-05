1430fa12f647ae3ed8404b346c8a694aa430aa76
amaydeveloper <amaydeveloper@MacBook-Pro-tilhrende-amaydeveloper.local>
2022-05-05 09:54:05 +0200

    added code to products and product components

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,39 @@
+import {
+  FavoriteBorderOutlined,
+  SearchOutlined,
+  ShoppingCartOutlined,
+} from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+ flex:1;
+ margin; 5px;
+`;
 
-const Product = ({ imte }) => {
-  return <Container></Container>;
+const Circle = styled.div``;
+const Info = styled.div``;
+const Image = styled.img``;
+const Icon = styled.div``;
+
+const Product = ({ item }) => {
+  return (
+    <Container>
+      <Circle />
+      <Image src={item.img} />
+      <Info>
+        <Icon>
+          <ShoppingCartOutlined />
+        </Icon>
+        <Icon>
+          <SearchOutlined />
+        </Icon>
+        <Icon>
+          <FavoriteBorderOutlined />
+        </Icon>
+      </Info>
+    </Container>
+  );
 };
 
 export default Product;
