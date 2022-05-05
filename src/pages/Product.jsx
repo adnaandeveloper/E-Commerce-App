1802f722dd2d684bc20480293519dc4f375b0227
amaydeveloper <amaydeveloper@MacBook-Pro-tilhrende-amaydeveloper.local>
2022-05-05 15:25:09 +0200

    Added Product Page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``;
+const Product = () => {
+  return (
+    <Container>
+      <h1>Hello</h1>
+    </Container>
+  );
+};
+
+export default Product;
