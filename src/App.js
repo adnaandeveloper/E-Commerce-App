7cbb6ce8025fb0cf5a0777ec336de1e41ed0edf0
amaydeveloper <amaydeveloper@MacBook-Pro-tilhrende-amaydeveloper.local>
2022-05-06 09:38:41 +0200

    Register page implemented

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
+import Register from './pages/Register';
 
 const App = () => {
   return (
     <div>
-      <Product />
+      <Register />
     </div>
   );
 };
